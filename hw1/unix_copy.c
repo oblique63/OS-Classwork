fe514445fb9b6a8fe4c93323d532b546d32eb1ba
Enrique <oblique63@gmail.com>
2012-02-03 14:29:13 -0800

    fixed unix_copy screen output; added readme

--- hw1/unix_copy.c
+++ hw1/unix_copy.c
@@ -4,7 +4,8 @@
 #include <fcntl.h>
 #include <string.h>
 
-#define buffer_size 11
+#define buffer_size  11
+#define max_filename 50
 
 
 // check if an expression is true, otherwise close the opened files and kill the program
@@ -19,31 +20,28 @@ void check(int expression, char *message, int in_fd, int out_fd) {
 
 // read user input, check for errors, and remove trailing new-line character from fgets result
 void get_input(char *buffer) {
-    // read input string, limiting the size of it to 'buffer_size',
-    // and catch the return value of the fgets call in case it fails
-    char *input = fgets(buffer, sizeof(char) * buffer_size, stdin);
+    // and catch the return value of the scanf call in case it fails
+    int input = scanf("%s", buffer);
 
     // check to make sure that 'fgets' didn't throw an error, otherwise exit the program
-    check(input != NULL, "Input read failed", -1, -1);
-
-    // remove the new-line character from the string
-    int length = strlen(buffer);
-    if (buffer[length-1] == '\n')
-        buffer[length-1] = '\0';
+    check(input != EOF, "[ERROR] Input read failed", -1, -1);
 }
 
 
 void copy_file(char *input_file, char *output_file) {
     // Open the input file with read permissions
     int in_fd = open(input_file, O_RDONLY);
-    check(in_fd != -1, "Failed to open input file", in_fd, -1);
+    check(in_fd != -1, "[ERROR] Failed to open input file", in_fd, -1);
 
     // Open the output file with write permissions (create it if it doesn't already exist),
     // and give the user read/write permissions as well
     int out_fd = open(output_file, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
-    check(out_fd != -1, "Failed to open output file", in_fd, out_fd);
+    check(out_fd != -1, "[ERROR] Failed to open output file", in_fd, out_fd);
 
-    char buffer[buffer_size];
+    // null-terminate the buffer
+    char buffer[buffer_size+1];
+    buffer[buffer_size] = '\0';
+    
     int bytes_read, bytes_written;
     do {
         // clear the buffer to avoid having any unwanted characters from the last read
@@ -51,15 +49,15 @@ void copy_file(char *input_file, char *output_file) {
 
         // read in 'buffer_size' bytes of data from the input file and store it in the buffer
         bytes_read = read(in_fd, buffer, buffer_size);
-        check(bytes_read != -1, "Failed to read input file", in_fd, out_fd);
+        check(bytes_read != -1, "[ERROR] Failed to read input file", in_fd, out_fd);
 
         // write the contents of the buffer to the output file, but limit it to the amount of
         // bytes actually read during the read system call
         bytes_written = write(out_fd, buffer, bytes_read);
-        check(bytes_written != -1, "Failed to write output file", in_fd, out_fd);
+        check(bytes_written != -1, "[ERROR] Failed to write output file", in_fd, out_fd);
         
         // print the contents of the buffer
-        printf("%s\0", buffer);
+        printf("%s", buffer);
 
     } while (bytes_read > 0);
 
@@ -68,19 +66,16 @@ void copy_file(char *input_file, char *output_file) {
 }
 
 
-int main(int argc, char *argv[]) {
-    
-    char input_file[buffer_size];
-    char output_file[buffer_size];
+int main() {
+    char input_file[max_filename];
+    char output_file[max_filename];
 
     puts("Welcome to the TEE Copy Program by Enrique Gavidia!");
     puts("Enter the name of the file to copy from:");
-    //get_input(input_file);
-    scanf("%s", input_file);
+    get_input(input_file);
     
     puts("Enter the name of the file to copy to:");
-    //get_input(output_file);
-    scanf("%s", output_file);
+    get_input(output_file);
 
     copy_file(input_file, output_file);
 
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Operating Systems Classwork
+
+This is just for organizing my work for an operating systems class I am taking. Nothing to see here, move along...
+
+
+## Disclaimer
+
+If you happen to be taking the same class as me, I am not responsible for the result of your wanderings around my code.
+Once you're done with the class however, feel free to fork/pull-request me if you want to add any 'improvements'. 
+Again, this is just for my own organizational purposes only.
+
+
+## Licensing
+This code is released under the ['Do What The Fuck You Want To' Public License (WTFPL)](http://sam.zoy.org/wtfpl/).  
+Copyright &copy; 2012, Enrique Gavidia
