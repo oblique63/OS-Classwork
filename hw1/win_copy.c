ac2cb0335194275fb004d638aa3968127a999bcf
Enrique Gavidia <oblique63@gmail.com>
2012-02-08 17:27:36 -0800

    finished windows implementation

--- hw1/win_copy.c
+++ hw1/win_copy.c
@@ -5,12 +5,7 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
-//#include <unistd.h>
-//#include <fcntl.h>
 #include <windows.h>
-#include <Winerror.h>
-#include <string.h>
 
 #define buffer_size  11
 #define max_filename 80
@@ -18,7 +13,8 @@
 // for the small input files
 #define debug_output 0
 
-// check if an expression is true, otherwise close the opened files and kill the program
+// check if an expression is true, otherwise close the opened files and exit the program with
+// the appropriate Windows error code.
 void check(int expression, char *message, HANDLE in_handle, HANDLE out_handle, int return_code) {
     if (!expression) {
         printf("%s: %x\n", message, GetLastError());
@@ -31,7 +27,7 @@ void check(int expression, char *message, HANDLE in_handle, HANDLE out_handle, i
 // read user input, check for errors, and remove trailing new-line character from fgets result
 void get_input(char *buffer) {
     // and catch the return value of the scanf call in case it fails
-    int input = scanf("%s", buffer);
+    int input = scanf("%s",buffer);
     // check to make sure that 'fgets' didn't throw an error, otherwise exit the program
     check(input != EOF, "[ERROR] Input read failed", NULL, NULL, ERROR_READ_FAULT);
 }
@@ -41,20 +37,19 @@ void copy_file(char *input_file, char *output_file) {
     HANDLE in_handle, out_handle;
     DWORD bytes_read, bytes_written, bytes_copied;
     BOOL syscall_success;
-    
     char buffer[buffer_size+1];
-    
-    // Open the input file with read permissions
-    in_handle = CreateFile(input_file, GENERIC_READ, FILE_SHARE_READ|FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+
+    // Open the input file, and make sure it exists
+    in_handle = CreateFile(input_file, GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL);
     check(in_handle != INVALID_HANDLE_VALUE, "[ERROR] Failed to open input file", in_handle, NULL, ERROR_FILE_NOT_FOUND);
 
-    // Open the output file with write permissions (create it if it doesn't already exist),
-    // and give the user read/write permissions as well
-    out_handle = CreateFile(input_file, GENERIC_WRITE, FILE_SHARE_READ|FILE_SHARE_WRITE, NULL, CREATE_ALWAYS, 0, NULL);
+    // Create the output file, and open it with write access
+    out_handle = CreateFile(output_file, GENERIC_WRITE, 0, NULL, CREATE_NEW, 0, NULL);
     check(out_handle != INVALID_HANDLE_VALUE, "[ERROR] Failed to open output file", in_handle, out_handle, ERROR_CANNOT_MAKE);
-    
+
     // ensure the buffer is always null-terminated by setting the last byte permanently to a null-character
     buffer[buffer_size] = '\0';
+    bytes_copied = 0;
     do {
         // clear the buffer to avoid having any unwanted characters from the last read
         memset(buffer, '\0', buffer_size);
@@ -62,14 +57,12 @@ void copy_file(char *input_file, char *output_file) {
         // read in 'buffer_size' bytes of data from the input file and store it in the buffer
         syscall_success = ReadFile(in_handle, buffer, buffer_size, &bytes_read, NULL);
         check(syscall_success != FALSE, "[ERROR] Failed to read input file", in_handle, out_handle, ERROR_READ_FAULT);
-        printf("READ %d bytes\n", bytes_read);
-        printf("BUFFER: %s\n", buffer);
-        
+
         // write the contents of the buffer to the output file, but limit it to the amount of
         // bytes actually read during the read system call
-        syscall_success = WriteFile(out_handle, buffer, bytes_read, &bytes_written, NULL);
+        syscall_success = WriteFile(out_handle, &buffer, bytes_read, &bytes_written, NULL);
         check(syscall_success != FALSE, "[ERROR] Failed to write output file", in_handle, out_handle, ERROR_WRITE_FAULT);
-        printf("WROTE %d bytes\n", bytes_written);
+
         bytes_copied += bytes_written;
 
         // print the contents of the buffer
@@ -83,7 +76,7 @@ void copy_file(char *input_file, char *output_file) {
 
     CloseHandle(in_handle);
     CloseHandle(out_handle);
-    printf("File copy successful, %d bytes copied\n", bytes_copied);
+    printf("\nFile copy successful, %d bytes copied\n", bytes_copied);
 }
 
 
