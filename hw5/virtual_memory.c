00d48fa053ef9b53571f5af79141e2f53b4f213f
Enrique <oblique63@gmail.com>
2012-04-13 00:32:56 -0700

    HW5: Implemented LRU (still untested...)

--- hw5/virtual_memory.c
+++ hw5/virtual_memory.c
@@ -28,12 +28,22 @@ int in_page_frame(int page_number) {
     return 0;
 }
 
+// pushes new frame index into stack, shift older numbers down.
+// Assumes stack is full, and of length 'number_of_frames'.
+int push(int *stack, int frame_index) {
+    int i;
+    for (i = 1; i < number_of_frames; i++)
+        stack[i-1] = stack[i];
+
+    stack[number_of_frames-1] = frame_index;
+}
 
 int FIFO() {
     int frame, reference_index, page_number, page_faults;
     int page_stored = 0;
 
     frame = 0;
+    reference_index = 0;
     for (page_faults = 0; reference_index < REFERENCE_SIZE; page_faults++) {
 
         page_stored = 0;
@@ -59,7 +69,37 @@ int FIFO() {
 
 int LRU() {
     int frame, reference_index, page_number, page_faults;
+    int least_referenced[number_of_frames];
+    int empty_frames = number_of_frames;
     int page_stored = 0;
+
+    reference_index = 0;
+    for (page_faults = 0; reference_index < REFERENCE_SIZE; page_faults++) {
+
+        page_stored = 0;
+        while (!page_stored && reference_index < REFERENCE_SIZE) {
+            page_number = page_reference[reference_index];
+
+            if (empty_frames > 0) {
+                frame = number_of_frames - empty_frames;
+                page_frames[frame] = page_number;
+                least_referenced[frame] = frame;
+            }
+
+            else {
+                frame = in_page_frame(page_number, number_of_frames);
+
+                if (!frame) {
+                    page_frames[ least_referenced[0] ] = page_number;
+                    page_stored = 1;
+                }
+
+                push(least_referenced, frame-1);
+            }
+
+            reference_index += 1;
+        }
+    }
 
     return page_faults;
 }
