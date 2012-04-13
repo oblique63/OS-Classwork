b7223e125efb50a7048fc63db83cad7a8681070a
Enrique <oblique63@gmail.com>
2012-04-12 21:33:10 -0700

    HW5: FIFO implemented for part 2 (untested)

--- hw5/virtual_memory.c
+++ hw5/virtual_memory.c
@@ -6,13 +6,9 @@
 #define MAX_PAGE_FRAMES 13
 
 int page_reference[REFERENCE_SIZE];
+int number_of_frames;
+int *page_frames;
 
-void check(int expression, char *message) {
-    if (!expression) {
-        perror(message);
-        exit(-1);
-    }
-}
 
 void populate_reference() {
     int i;
@@ -21,14 +17,75 @@ void populate_reference() {
     }
 }
 
-void FIFO() {}
+int in_page_frame(int page_number) {
+    int frame;
+    for (frame = 0; frame < number_of_frames; frame++) {
+        // Return frame number for LRU reference counting purposes
+        if (page_frames[frame] == page_number)
+            return frame+1;
+    }
 
-void LRU() {}
+    return 0;
+}
 
-void OPT() {}
+
+int FIFO() {
+    int frame, reference_index, page_number, page_faults;
+    int page_stored = 0;
+
+    frame = 0;
+    for (page_faults = 0; reference_index < REFERENCE_SIZE; page_faults++) {
+
+        page_stored = 0;
+        while (!page_stored && reference_index < REFERENCE_SIZE) {
+            page_number = page_reference[reference_index];
+
+            if (!in_page_frame(page_number, number_of_frames)) {
+                page_frames[frame] = page_number;
+                page_stored = 1;
+            }
+
+            reference_index += 1;
+        }
+
+        if (frame < number_of_frames)
+            frame += 1;
+        else
+            frame = 0;
+    }
+
+    return page_faults;
+}
+
+int LRU() {
+    int frame, reference_index, page_number, page_faults;
+    int page_stored = 0;
+
+    return page_faults;
+}
+
+int OPT() {
+    int frame, reference_index, page_number, page_faults;
+    int page_stored = 0;
+
+    return page_faults;
+}
+
 
 int main() {
     populate_reference();
+
+    for (number_of_frames = 1; number_of_frames <= MAX_PAGE_FRAMES; number_of_frames++) {
+
+        page_frames = calloc(number_of_frames, sizeof(int));
+
+        printf("\n=== Using %d Page Frames ===\n", number_of_frames);
+        printf("FIFO: %d page faults\n", FIFO(number_of_frames));
+        printf("LRU:  %d page faults\n", LRU(number_of_frames));
+        printf("OPT:  %d page faults\n", OPT(number_of_frames);
+
+        free(page_frames);
+    }
 
     return 0;
 }
