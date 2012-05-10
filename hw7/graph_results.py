3cdd5da233cebcd1e124488363fdf35ccf7aac3f
Enrique Gavidia <oblique63@gmail.com>
2012-05-09 20:21:38 -0700

    HW7: collected windows singlecore results; started writeup

--- hw7/graph_results.py
+++ hw7/graph_results.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 from matplotlib.mlab import csv2rec
 
-unix_single = csv2rec('output/unix_search_singlecore.csv', delimiter=',')
-unix_multi = csv2rec('output/unix_search_multicore.csv', delimiter=',')
+unix_single = csv2rec('output/csv/unix_singlecore.csv', delimiter=',')
+unix_multi = csv2rec('output/csv/unix_multicore.csv', delimiter=',')
 
-win_single = csv2rec('output/win_search_singlecore.csv', delimiter=',')
-win_multi = csv2rec('output/win_search_multicore.csv', delimiter=',')
+win_single = csv2rec('output/csv/win_singlecore.csv', delimiter=',')
+win_multi = csv2rec('output/csv/win_multicore.csv', delimiter=',')
 
 # Page Faults vs Number of Frames
 #plt.yscale('log', basey=2)
@@ -15,7 +15,7 @@ plt.ylabel("Time (micro-seconds)")
 
 plt.title("Unix Single-core")
 plt.plot(unix_single.threads, unix_single.time, color='g', linestyle='-', marker='o')
-plt.savefig('output/unix_singlecore_graph')
+plt.savefig('output/graphs/unix_singlecore_graph')
 plt.close()
 
 #plt.yscale('log', basey=2)
@@ -24,7 +24,7 @@ plt.ylabel("Time (micro-seconds)")
 
 plt.title("Unix Dual-core")
 plt.plot(unix_multi.threads, unix_multi.time, color='r', linestyle='-', marker='o')
-plt.savefig('output/unix_multicore_graph')
+plt.savefig('output/graphs/unix_multicore_graph')
 plt.close()
 
 #plt.yscale('log', basey=2)
@@ -33,7 +33,7 @@ plt.ylabel("Time (micro-seconds)")
 
 plt.title("Windows Single-core")
 plt.plot(win_single.threads, win_single.time, color='b', linestyle='-', marker='o')
-plt.savefig('output/win_singlecore_graph')
+plt.savefig('output/graphs/win_singlecore_graph')
 plt.close()
 
 #plt.yscale('log', basey=2)
@@ -42,5 +42,5 @@ plt.ylabel("Time (micro-seconds)")
 
 plt.title("Windows Dual-core")
 plt.plot(win_multi.threads, win_multi.time, color='y', linestyle='-', marker='o')
-plt.savefig('output/win_multicore_graph')
+plt.savefig('output/graphs/win_multicore_graph')
 plt.close()
